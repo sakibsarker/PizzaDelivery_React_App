e2a4680c1d2ce84239e8b1d939859ad4f41235f7
sakibsarker <sakib15-3824@diu.edu.bd>
2022-05-23 16:07:01 +0600

    banner change

--- src/components/React_bootsrap/Header.js
+++ src/components/React_bootsrap/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="banner__content">
         <div className='container'>
             <div className="banner__text">
-            <h3>Pizza Delivery</h3>
+            <h3>Pizz Delivery</h3>
             <h1>MAESTRO PIZZINNI</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Quibusdam quos cum aliquid maxime ea.</p>
